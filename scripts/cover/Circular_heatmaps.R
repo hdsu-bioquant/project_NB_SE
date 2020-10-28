61f47018e5b0b1702ba4a14d6602887e9d7aec8c
Andres Quintero <a.quintero@dkfz.de>
2020-10-28 11:43:15 +0100

    cover init commit

---
+++ scripts/cover/Circular_heatmaps.R
@@ -0,0 +1,26 @@
+library(circlize)
+library(ComplexHeatmap)
+library(viridis)
+
+hmatrix_wnorm<- readRDS("analysis/tumor/chipseq/H3K27ac/NMF/tumor_consensusSE_K4_Hmatrix_wnorm.RDS")
+
+
+
+Heatmap(hmatrix_wnorm,
+        col  = viridis(n=100),
+        name = "Exposure",
+        clustering_distance_columns = 'pearson',
+        show_column_dend = FALSE,
+        show_column_names = FALSE,
+        show_row_names = FALSE,
+        cluster_rows = FALSE)
+
+col_fun1 <- colorRamp2(seq(min(hmatrix_wnorm), max(hmatrix_wnorm), length.out = 100), 
+                       viridis(n=100))
+
+circos.par
+circos.par$gap.after <- 0
+circos.clear()
+circos.heatmap(t(hmatrix_wnorm),
+               col = col_fun1,
+               track.height = .8)
